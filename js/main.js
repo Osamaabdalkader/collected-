fa9ca1fd4e74972ad0404e7b75cda05785350c92
Osamaabdalkader <osamaabdalkader100@gmail.com>
2025-09-17 21:28:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 // js/main.js - الملف الرئيسي للتطبيق
 import { loadComponent } from './utils/componentLoader.js';
+import { initHeader } from './components/header-main.js';
+import { initFooter } from './components/footer-common.js';
+import { initHomePage } from './pages/home.js';
 
 // تهيئة التطبيق عند تحميل الصفحة
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         // تحميل الهيدر
         await loadComponent('header-container', 'components/header-main.html');
+        initHeader();
         
         // تحميل الفوتر
         await loadComponent('footer-container', 'components/footer-common.html');
+        initFooter();
+        
+        // تهيئة الصفحة الرئيسية
+        initHomePage();
         
         console.log('تم تحميل المكونات بنجاح');
     } catch (error) {
